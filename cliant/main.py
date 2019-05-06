4b1aece0c419bf21c1ce75852ff436def2dd24b3
kerbalwzy <kerbalwzy@gmail.com>
2019-05-06 18:02:07 +0800

    begin client window

--- cliant/main.py
+++ cliant/main.py
@@ -1,8 +1,17 @@
 import tkinter as tk
 
-root = tk.Tk()
-tk.Label(master=root, text="文字").pack()
-tk.Label(master=root, bitmap='warning').pack()
+
+class ClientRootWindow(tk.Tk):
+
+    def create_window(self):
+        self.title = "ScreenMonitorClient"
+        screen_w, screen_h = self.maxsize()
+        w = int(screen_w*0.618)
+        h = int(screen_h*0.618)
+        self.geometry("{}x{}".format(w, h))
+
 
 if __name__ == '__main__':
-    root.mainloop()
+    client = ClientRootWindow()
+    client.create_window()
+    client.mainloop()
